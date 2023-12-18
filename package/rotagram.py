b56c847e98fb51af280baeed5a18ec25c6fd8b41
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2023-12-18 15:12:04 +0100

    Update rotagram.py

--- package/rotagram.py
+++ package/rotagram.py
@@ -7,12 +7,12 @@ import pandas as pd
 import os
 
 
-def rotagram(steps_lim_bis, segm, signal_tr, output):
+def rotagram(steps_lim_bis, segm, data_lb, output):
     segm = pd.DataFrame(segm)
     os.chdir(output)
-    # Définition de la figure et des couleurs
+    
+    # figure and color definition
     fig, ax = plt.subplots(1, 3, gridspec_kw={'width_ratios': [20, 1, 1]}, figsize=(12, 10))
-    # plt.ion()
     ax[1].set_title('        Stance', fontsize=10, weight='bold')
     ax[2].set_title('phase       ', fontsize=10, weight='bold')
     ax[0].set_yticks([])
@@ -25,60 +25,56 @@ def rotagram(steps_lim_bis, segm, signal_tr, output):
     color_r, line_r = 'blue', '-'
     color_l, line_l = 'red', '-'
 
-    # Un tableau pour le pied droit, un pour le pied gauche
-    step_r = steps_lim_bis[steps_lim_bis["Foot"]== 1]
-    step_l = steps_lim_bis[steps_lim_bis["Foot"] == 0]
+    # one table for the right foot, one for the left foot
+    events_right = steps_lim_bis[steps_lim_bis["Foot"]== 1]
+    events_left = steps_lim_bis[steps_lim_bis["Foot"] == 0]
 
-    t = signal_tr["PacketCounter"] - segm.iloc[1, 0]/ 100
-    sc = - signal_tr.iloc[:, 8]  # Le négatif sert à avoir la droite en haut
+    t = data_lb["PacketCounter"] - segm.iloc[1, 0]/ 100
+    sc = - data_lb["Gyr_X"]
 
-    # Courbe cumulée noire fine
-    courbe_fine = np.cumsum(sc[segm.iloc[1, 0]-100: segm.iloc[2, 0]+100])
-    coef = np.sign(courbe_fine.iloc[-1])* 180 / courbe_fine.iloc[-1]
-    courbe_fine = np.sign(courbe_fine.iloc[-1]) * courbe_fine * 180 / courbe_fine.iloc[-1]
-    leg3 = ax[0].plot(courbe_fine, t[segm.iloc[1, 0]-100: segm.iloc[2, 0]+100], 'k')
+    # Fine black cumulative curve for u-turn
+    cumulative_curve = np.cumsum(sc[segm.iloc[1, 0]-100: segm.iloc[2, 0]+100])
+    coef = np.sign(cumulative_curve.iloc[-1])* 180 / cumulative_curve.iloc[-1]
+    cumulative_curve = np.sign(cumulative_curve.iloc[-1]) * cumulative_curve * 180 / cumulative_curve.iloc[-1]
+    leg3 = ax[0].plot(cumulative_curve, t[segm.iloc[1, 0]-100: segm.iloc[2, 0]+100], 'k')
 
-    W = abs(min(courbe_fine))
-    W1 = abs(max(courbe_fine))
+    W = abs(min(cumulative_curve))
+    W1 = abs(max(cumulative_curve))
     if W1 > W:
         W = W1
-    W = max(abs(courbe_fine))
+    W = max(abs(cumulative_curve))
 
-    if max(courbe_fine) > 100:
+    if max(cumulative_curve) > 100:
         ax[0].annotate('U-turn to the right', xy=(2, 1), xytext=(30, (-1)), fontsize=10, weight='bold')
     else:
         ax[0].annotate('U-turn to the left', xy=(2, 1), xytext=(-W + 30, (-1)), fontsize=10, weight='bold')
 
 
-    # Plot de la rotation
-    for y in range(len(step_r)-1):
-        if (step_r["TO"].tolist()[y+1] - segm.iloc[1, 0])*(step_r["HS"].tolist()[y] - segm.iloc[1, 0]) > 0:
-            # Premier plot
-            leg_rf = ([step_r["HS"].tolist()[y], step_r["TO"].tolist()[y+1]] - segm.iloc[1, 0]) / 100
+    # rotation plot for each stance phase
+    for y in range(len(events_right)-1):
+        if (events_right["TO"].tolist()[y+1] - segm.iloc[1, 0])*(events_right["HS"].tolist()[y] - segm.iloc[1, 0]) > 0:
+            # plot
+            leg_rf = ([events_right["HS"].tolist()[y], events_right["TO"].tolist()[y+1]] - segm.iloc[1, 0]) / 100
             leg1 = ax[2].plot([0, 0], leg_rf, line_r, linewidth=3, color=color_r)
 
-            ax[0].plot(-np.cumsum(sc[int(step_r["HS"].tolist()[y]):int(step_r["TO"].tolist()[y+1])]) * coef,
-                       t[int(step_r["HS"].tolist()[y]):int(step_r["TO"].tolist()[y+1])],
+            ax[0].plot(-np.cumsum(sc[int(events_right["HS"].tolist()[y]):int(events_right["TO"].tolist()[y+1])]) * coef,
+                       t[int(events_right["HS"].tolist()[y]):int(events_right["TO"].tolist()[y+1])],
                        line_r, linewidth=3, color=color_r)
 
-    for y in range(len(step_l)-1):
-        if (step_l["TO"].tolist()[y+1] - segm.iloc[1, 0])*(step_l["HS"].tolist()[y] - segm.iloc[1, 0]) > 0:
-            # leg_lf = ([step_l["HS"].tolist()[y]  - len(signal_tr), step_l["TO"].tolist()[y+1] - len(signal_tr)] - segm.iloc[1, 0]) / 100
-            leg_lf = ([step_l["HS"].tolist()[y], step_l["TO"].tolist()[y+1]] - segm.iloc[1, 0]) / 100
+    for y in range(len(events_left)-1):
+        if (events_left["TO"].tolist()[y+1] - segm.iloc[1, 0])*(events_left["HS"].tolist()[y] - segm.iloc[1, 0]) > 0:
+            # leg_lf = ([events_left["HS"].tolist()[y]  - len(data_lb), events_left["TO"].tolist()[y+1] - len(data_lb)] - segm.iloc[1, 0]) / 100
+            leg_lf = ([events_left["HS"].tolist()[y], events_left["TO"].tolist()[y+1]] - segm.iloc[1, 0]) / 100
             leg2 = ax[1].plot([0, 0], leg_lf, line_r, linewidth=3, color=color_l)
 
-            #print("len", len(t), int(step_l["HS"][y]),int(step_l["TO"][y+1]), len(sc[int(step_l["HS"][y] - len(signal_tr)):int(step_l["TO"][y+1] - len(signal_tr))]), len(leg_lf))
-            #print(np.cumsum(sc[int(step_l["HS"][y] - len(signal_tr)):int(step_l["TO"][y+1] - len(signal_tr))]) * coef)
-            #print(t[int(step_l["HS"][y] - len(signal_tr)):int(step_l["TO"][y+1] - len(signal_tr))])
-
-            #ax[0].plot(np.cumsum(sc[int(step_l["HS"].tolist()[y] - len(signal_tr)):int(step_l["TO"].tolist()[y+1] - len(signal_tr))]) * coef,
-             #          t[int(step_l["HS"].tolist()[y] - len(signal_tr)):int(step_l["TO"].tolist()[y+1] - len(signal_tr))],
+            #ax[0].plot(np.cumsum(sc[int(events_left["HS"].tolist()[y] - len(data_lb)):int(events_left["TO"].tolist()[y+1] - len(data_lb))]) * coef,
+             #          t[int(events_left["HS"].tolist()[y] - len(data_lb)):int(events_left["TO"].tolist()[y+1] - len(data_lb))],
               #         line_l, linewidth=3, color=color_l)
-            ax[0].plot(np.cumsum(sc[int(step_l["HS"].tolist()[y]):int(step_l["TO"].tolist()[y+1])]) * coef,
-                       t[int(step_l["HS"].tolist()[y]):int(step_l["TO"].tolist()[y+1])],
+            ax[0].plot(np.cumsum(sc[int(events_left["HS"].tolist()[y]):int(events_left["TO"].tolist()[y+1])]) * coef,
+                       t[int(events_left["HS"].tolist()[y]):int(events_left["TO"].tolist()[y+1])],
                        line_l, linewidth=3, color=color_l)
 
-        # Coloration des aires de la figure
+        # coloring the areas of the figure
         ax[0].add_patch(
             patches.Rectangle(
                 (-W, (segm.iloc[0, 0] - segm.iloc[1, 0]) / 100),  # (x,y)
@@ -147,22 +143,14 @@ def rotagram(steps_lim_bis, segm, signal_tr, output):
         )
 
         # Légendes
-        # ax[0].title("Angle de rotation du tronc" + ref, fontsize=12)
-        # axd = ax[0].twiny()
+        fig.suptitle('Rotagram', fontsize=13)
         ax[0].set_xticks([-W, (-W / 2), 0, W / 2, W])
-        # ax[0].set_xticklabels(['Demi-tour vers la gauche','Demi-tour vers la droite'], fontsize=8)
         ax[0].set_xticklabels(['180°', '90°', '0°', '90°', '180°'], fontsize=8)
-        # axd.set_xlim(ax[0].get_xlim())
-        #axd.set_xticks([0])
         ax[0].set_title('Trunk rotation angle', weight='bold', size=10)
 
         ax[0].tick_params(axis=u'both', which=u'both', length=0)
-        # ax[0].set_yticks([((segm[0] - segm[1]) / 100)+1.1, -0.5,((segm[2] - segm[1]) / 100)/2,((segm[2] - segm[1]) / 100)+0.5,((segm[3] - segm[1]) / 100)-1.1])
         e = str(((segm.iloc[2, 0] - segm.iloc[1, 0]) / 100)) + 's.'
-        #  ax[0].set_yticklabels(['Debut\nde la\nmarche','Debut \ndemi-tour',e,'Fin\ndemi-tour','Fin\nde la\nmarche'], fontsize=8)
-        # # ax[0].set_ylabel('Pied gauche'),rotation=90
         ax[1].tick_params(axis=u'both', which=u'both', length=0)
-        # ax[0].spines['left'].set_visible(False)
         ax[1].spines['right'].set_visible(False)
         ax[2].set_yticks(
             [((segm.iloc[0, 0] - segm.iloc[1, 0]) / 100) + 1.1, -0.5, ((segm.iloc[2, 0] - segm.iloc[1, 0]) / 100) / 2,
@@ -175,14 +163,9 @@ def rotagram(steps_lim_bis, segm, signal_tr, output):
         ax[2].spines['left'].set_visible(False)
         ax[2].yaxis.tick_right()
 
-        # ax.set_title('U-Turn Rotagram for ' + ref, fontsize=16)
-        # ax.legend([leg1[0], leg2[0], leg3[0]], ['right', 'left', 'cumulative_angle'])
-
-        # fig.set_size_inches(20, 12)
-
-    # On est déjà dans le bon répertoire
-    plt.show()
-    plt.ioff()
+        ax[0].legend([leg1[0], leg2[0], leg3[0]], ['right', 'left', 'cumulative_angle'])
+    
+    # save fig
     plt.savefig(fname=("rota.svg"))
 
     return None
