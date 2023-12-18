aef5c12aab2b8da402a1ae306251ee3db230a0ef
CyrilVoisard <62250759+CyrilVoisard@users.noreply.github.com>
2023-12-18 13:44:07 +0100

    Update rotagram.py

--- package/rotagram.py
+++ package/rotagram.py
@@ -54,7 +54,7 @@ def rotagram(steps_lim_bis, segm, signal_tr, output):
     for y in range(len(step_r)):
         print("test", step_r["HS"][y], step_r["TO"][y])
         print("test", segm.iloc[3, 0])
-        if (step_r.iloc[y, 3] < segm.iloc[3, 0]):
+        if (step_r["HS"][y] < segm.iloc[3, 0]):
             # Premier plot
             leg_rf = ([step_r["HS"][y], step_r["TO"][y]] - segm.iloc[1, 0]) / 100
             leg1 = ax[2].plot([0, 0], leg_rf, line_r, linewidth=3, color=color_r)
